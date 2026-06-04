962c09fe1cdcc5bfca72b0546dcb6f4bbb179997
ayusar <work38896@gmail.com>
2026-06-04 17:55:01 +0530

    Update client.go

--- internal/bot/client.go
+++ internal/bot/client.go
@@ -46,7 +46,7 @@ func StartClient(log *zap.Logger) (*gotgproto.Client, error) {
 		if result.err != nil {
 			return nil, result.err
 		}
-		commands.Load(log, result.client.Dispatcher)
+		commands.Load(log, result.client.Dispatcher, result.client)
 		log.Info("Client started", zap.String("username", result.client.Self.Username))
 		Bot = result.client
 		return result.client, nil
