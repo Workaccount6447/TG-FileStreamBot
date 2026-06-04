80de7c9a8f57d22ac0de5e388d18cf9fcf4e9597
ayusar <work38896@gmail.com>
2026-06-04 17:54:11 +0530

    Update commands.go

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -3,20 +3,22 @@ package commands
 import (
 	"reflect"
 
+	"github.com/celestix/gotgproto"
 	"github.com/celestix/gotgproto/dispatcher"
 	"go.uber.org/zap"
 )
 
 type command struct {
-	log *zap.Logger
+	log    *zap.Logger
+	client *gotgproto.Client
 }
 
-func Load(log *zap.Logger, dispatcher dispatcher.Dispatcher) {
+func Load(log *zap.Logger, dispatcher dispatcher.Dispatcher, client *gotgproto.Client) {
 	log = log.Named("commands")
 	defer log.Info("Initialized all command handlers")
-	Type := reflect.TypeOf(&command{log})
-	Value := reflect.ValueOf(&command{log})
+	cmd := &command{log: log, client: client}
+	Type := reflect.TypeOf(cmd)
+	Value := reflect.ValueOf(cmd)
 	for i := 0; i < Type.NumMethod(); i++ {
 		Type.Method(i).Func.Call([]reflect.Value{Value, reflect.ValueOf(dispatcher)})
 	}
-}
