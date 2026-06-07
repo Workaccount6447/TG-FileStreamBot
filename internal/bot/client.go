40342ab52895701981e85a7e1577c9fb67370064
ayusar <work38896@gmail.com>
2026-06-07 08:17:36 +0530

    Implement bot command registration
    Added setBotCommands function to register bot commands.

--- internal/bot/client.go
+++ internal/bot/client.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/celestix/gotgproto"
 	"github.com/celestix/gotgproto/sessionMaker"
+	"github.com/gotd/td/tg"
 )
 
 var Bot *gotgproto.Client
@@ -49,6 +50,23 @@ func StartClient(log *zap.Logger) (*gotgproto.Client, error) {
 		commands.Load(log, result.client.Dispatcher, result.client)
 		log.Info("Client started", zap.String("username", result.client.Self.Username))
 		Bot = result.client
+		// FIX Bug C: setBotCommands was defined but never called.
+		// Register /start and /myfiles in Telegram's command menu.
+		go setBotCommands(result.client)
 		return result.client, nil
 	}
+}
+
+func setBotCommands(client *gotgproto.Client) {
+	ctx := client.CreateContext()
+	ctx.Raw.BotsSetBotCommands(ctx, &tg.BotsSetBotCommandsRequest{
+		Scope:    &tg.BotCommandScopeDefault{},
+		LangCode: "",
+		Commands: []tg.BotCommand{
+			{Command: "start", Description: "Start the bot"},
+			{Command: "myfiles", Description: "Browse your uploaded files with stream links"},
+			{Command: "stats", Description: "View your personal usage statistics"},
+			{Command: "clearfiles", Description: "Delete all your files from history"},
+		},
+	})
 }
